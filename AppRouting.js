40b36cc71ed286b5dd053a38e31ecc0b04d3645b
theactofshining <ruslan.e.valikhanov@gmail.com>
2022-05-23 11:20:12 +0200

    dbresult

--- AppRouting.js
+++ AppRouting.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const pool = require("./db/client");
 require("dotenv").config();
 require("./db/client");
 
@@ -6,6 +7,15 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 app.use(express.json());
+
+app.get("/", async (req, res) => {
+    try{
+    const dbresult = await pool.query("SELECT * FROM users");
+    console.log(dbresult);
+    }catch(err){
+        console.log(err)
+    }
+});
 
 app.listen(port, () => {
     console.log(`App is listening on port ${port}`);
