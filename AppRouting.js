f532f2d7c0c25cf01c6f196a0426e6e5a9ded892
theactofshining <ruslan.e.valikhanov@gmail.com>
2022-05-24 10:24:38 +0200

    put request

--- AppRouting.js
+++ AppRouting.js
@@ -22,7 +22,7 @@ app.get("/:id", async (req, res) => {
     const { id } = req.params;
     try {
         const { rowCount, rows } = await pool.query("SELECT * FROM users WHERE id = $1", [id]);
-        if (!rowCount) return res.status(404).json({error: `Artcile with id of ${id} wasnt found`});
+        if (!rowCount) return res.status(404).json({ error: `Artcile with id of ${id} wasnt found` });
         res.status(200).send(rows[0]);
     } catch (err) {
         res.status(500).send(err);
@@ -30,20 +30,41 @@ app.get("/:id", async (req, res) => {
 });
 
 app.post("/", body("age").isFloat({ min: 0, max: 120 }),
-
     async (req, res) => {
-        const { body: {first_name, last_name, age}
-    } = req;
+        const { body: { first_name, last_name, age }
+        } = req;
         if (!first_name || !last_name || !age)
-        return res.status(400).send({ error: "Type smth!"});
+            return res.status(400).send({ error: "Type smth!" });
         const errors = validationResult(req);
         if (!errors.isEmpty()) {
             return res.status(400).json({ errors: "Sad truth: people don't live for so long" });
         }
-
         try {
             const { rows } = await pool.query("INSERT INTO users (first_name, last_name, age) VALUES ($1, $2, $3) RETURNING *",
                 [req.body.first_name, req.body.last_name, req.body.age]);
+            res.status(201).send(rows)
+        } catch (err) {
+            res.status(411).send(err);
+        }
+    });
+
+app.put("/:id", body("age").isFloat({ min: 0, max: 120 }),
+    async (req, res) => {
+        const { params: { id }
+        } = req;
+        if (!id)
+            return res.status(404).send({ error: "ID not found!" });
+        const { body: { first_name, last_name, age }
+        } = req;
+        if (!first_name || !last_name || !age)
+            return res.status(400).send({ error: "Type smth!" });
+        const errors = validationResult(req);
+        if (!errors.isEmpty()) {
+            return res.status(400).json({ errors: "Sad truth: people don't live for so long" });
+        }
+        try {
+            const { rows } = await pool.query("UPDATE users SET first_name=$1, last_name=$2, age=$3 WHERE id=$4 RETURNING *",
+                [first_name, last_name, age, id]);
             res.status(201).send(rows)
         } catch (err) {
             res.status(411).send(err);
